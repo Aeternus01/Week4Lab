1f70fd0fa9458a0d3a39a7b3e20443cd9c344cf9
Aeternus01 <pikmen1234@gmail.com>
2023-09-03 23:08:40 +1000

    100%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     private int i;
+    private int j;
     public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        j = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
-        Debug.Log("" + this.name + ":" + i);
+        Debug.Log("" + gameObject.name + ":" + i);
+        if (gameObject.tag == "Red" && i == 100){
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" && i == j){
+            rend.enabled = false;
+        }
     }
 }
